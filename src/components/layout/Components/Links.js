b25a2ef59c9f56e93f59b78976ab7e59d1a3a209
akashkumarkyurius <akashkumar.s@kyurius.tech>
2024-09-04 12:08:01 +0530

    NavBarLatestPush

--- src/components/layout/Components/Links.js
+++ src/components/layout/Components/Links.js
@@ -21,7 +21,7 @@ const LinkArray = [
 ];
 export default function Links() {
   return (
-    <ul className=" flex font-Roboto gap-2 justify-between lg:text-[#8a879f] font-medium  lg:flex-row flex-col">
+    <ul className=" flex font-Roboto gap-2 justify-between lg:text-white font-medium  lg:flex-row flex-col">
       {LinkArray.map((item) => (
         <li key={item.name} className="lg:my-0 my-5 lg:mx-0 mx-2 ">
           {item.name}
